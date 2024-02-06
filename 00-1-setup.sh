757384211a0a00b405b201876a7459fad150794b
Quinn Coxon <93389237+vespaquinn@users.noreply.github.com>
2024-02-06 20:43:47 +0100

    Update 00-1-setup.sh

--- 00-1-setup.sh
+++ 00-1-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=1G
-#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=02:00:00                      # NB. this may need to be increased for larger datasets
 #SBATCH --job-name=setup
 #SBATCH --output=/PATH/TO/OUTPUT/DIR/setup.o # !! CHANGE !!
 #SBATCH --error=/PATH/TO/OUTPUT/DIR/setup.e # !! CHANGE !!
